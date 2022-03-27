a1fe56705ee9ed721a5da0d43faa815cffd54d33
seong joo kim <iamseongjoo@gmail.com>
2022-03-27 16:58:10 +0900

    board pointPayment task entities

---
+++ backend/src/apis/board/entities/ board.entity.ts
@@ -0,0 +1,35 @@
+import { Field, ObjectType } from "@nestjs/graphql";
+import { Project } from "src/apis/project/entities/project.entity";
+import { User } from "src/apis/user/entities/user.entity";
+import { Column, CreateDateColumn, DeleteDateColumn, Entity, ManyToOne, PrimaryGeneratedColumn, UpdateDateColumn } from "typeorm";
+
+@Entity()
+@ObjectType()
+export class Board {
+    @PrimaryGeneratedColumn('uuid')
+    @Field(() => String)
+    id: string;
+
+    @Column()
+    @Field(() => String)
+    title: string;
+
+    @Column()
+    @Field(() => String)
+    content: string;
+
+    @CreateDateColumn()
+    createdAt: Date
+
+    @UpdateDateColumn()
+    updatedAt: Date
+
+    @DeleteDateColumn()
+    deletedAt: Date
+
+    @ManyToOne(() => User)
+    user: User
+ 
+    @ManyToOne(() => Project)
+    project: Project
+}
